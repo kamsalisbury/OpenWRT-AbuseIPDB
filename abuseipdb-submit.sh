8291a8756f451ca1dbe0dc2757db594d979a2e13
Kam Salisbury <29674098+kamsalisbury@users.noreply.github.com>
2024-02-11 08:39:56 -0500

    Sometimes the best tool is sed

--- abuseipdb-submit.sh
+++ abuseipdb-submit.sh
@@ -1,18 +1,22 @@
 #!/bin/ash
-
-# The following variable does not work from inside the awk statements as written.
-# The current OpenWRT date format is a calendar month instead of a numeric month.
-# Have to create a conversion from calendar month to numeric to achieve full automation. 
-month=$(date +%m)
-
+# For the latest script, reference https://github/kamsalisbury/OpenWRT-AbuseIPDB
 logread -Z 3 \
 | grep DPT \
-| awk '{print $15, $24, $5, $3, $4}' \
+| awk '{print $15, $24, $5, $2, $3, $4}' \
 | grep DPT \
 | awk '{gsub(/SRC=|DPT=/, ""); print}' \
 | awk '!seen[$1]++' \
-| awk '{print "curl https://api.abuseipdb.com/api/v2/report \-\-data\-urlencode ip\="$1 " \-d categories\=14 \-\-data\-urlencode \"comment\=Portscan "$2"\" \-\-data\-urlencode \"timestamp\="$3"\-02\-"$4"T"$5"-05:00\" \-H \"Key: <AbuseIPDB API v2 key>\" \-H \"Accept: application/json\""}' \
+| sed 's/Jan/01/g' \
+| sed 's/Feb/02/g' \
+| sed 's/Mar/03/g' \
+| sed 's/Apr/04/g' \
+| sed 's/May/05/g' \
+| sed 's/Jun/06/g' \
+| sed 's/Jul/07/g' \
+| sed 's/Aug/08/g' \
+| sed 's/Sep/09/g' \
+| sed 's/Oct/10/g' \
+| sed 's/Nov/11/g' \
+| sed 's/Dec/12/g' \
+| awk '{print "curl https://api.abuseipdb.com/api/v2/report \-\-data\-urlencode ip\="$1 " \-d categories\=14 \-\-data\-urlencode \"comment\=Portscan "$2"\" \-\-data\-urlencode \"timestamp\="$3"\-"$4"\-"$5"T"$6"-05:00\" \-H \"Key: <Your AbuseIPDB API Key>\" \-H \"Accept: application/json\""}' \
 | sh
-
-# OpenWRT uses syslogd to a ring buffer, so the log will rollover when the buffer fills up.
-# On my system the rollover happens aproximately every hour and half. YMMV.

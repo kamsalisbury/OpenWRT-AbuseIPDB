f8b5e5b95636f2a9c4bfc67ff512af0e7bb66b7f
Kam Salisbury <29674098+kamsalisbury@users.noreply.github.com>
2024-03-05 19:37:02 -0500

    Additional sed

--- abuseipdb-submit.sh
+++ abuseipdb-submit.sh
@@ -18,5 +18,14 @@ logread -Z 3 \
 | sed 's/Oct/10/g' \
 | sed 's/Nov/11/g' \
 | sed 's/Dec/12/g' \
+| sed 's/ 1 / 01 /g' \
+| sed 's/ 2 / 02 /g' \
+| sed 's/ 3 / 03 /g' \
+| sed 's/ 4 / 04 /g' \
+| sed 's/ 5 / 05 /g' \
+| sed 's/ 6 / 06 /g' \
+| sed 's/ 7 / 07 /g' \
+| sed 's/ 8 / 08 /g' \
+| sed 's/ 9 / 09 /g' \
 | awk '{print "curl https://api.abuseipdb.com/api/v2/report \-\-data\-urlencode ip\="$1 " \-d categories\=14 \-\-data\-urlencode \"comment\=Portscan "$2"\" \-\-data\-urlencode \"timestamp\="$3"\-"$4"\-"$5"T"$6"-05:00\" \-H \"Key: <Your AbuseIPDB API Key>\" \-H \"Accept: application/json\""}' \
 | sh
